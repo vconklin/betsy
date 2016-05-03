bd539294fef42ce1449e4ec28a4c87c0cc59b264
Nadine Curry <nlcurry@gmail.com>
2016-05-03 15:35:07 -0700

    Started on sign up page. mostly done

--- db/schema.rb
+++ db/schema.rb
@@ -55,11 +55,11 @@ ActiveRecord::Schema.define(version: 20160502231600) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   null: false
-    t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "username",        null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
