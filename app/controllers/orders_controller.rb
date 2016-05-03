95d388a388dc9c7879d4a746585900e3020da5ca
Risha <rishallen@gmail.com>
2016-05-03 14:36:46 -0700

    not sure

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
   def index
+    @cart  = Cart.find(params[:id])
+    @orders = @cart.orders
+    @total =  
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502231600) do
+ActiveRecord::Schema.define(version: 20160503211443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "order_id",   null: false
