24f6c93c34c64337f9b0e1be9ddcca5abf35b0b5
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-03 14:33:04 -0700

    add associations between product and review models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ActiveRecord::Base
-  has_many :reviews
+<<<<<<< 5f581a6c3ec6d3e3f6a5262a37986339bba7d3b4
   has_many :order_items
   belongs_to :user
+  has_many :reviews
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than: 0}
 
+=======
+>>>>>>> add associations between product and review models
 end
