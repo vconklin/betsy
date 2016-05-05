6fddc434c462518c65ff9724b90a31af44c2aaeb
Valerie <val.conklin@gmail.com>
2016-05-04 23:03:18 -0700

    Made it so cart-viewing is done through orderitems controller, so that orders_controller can just focus on the checkout

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,15 @@
 class OrderItemsController < ApplicationController
+
   def index
+    @order = Order.find(session[:order_id])
+    @order_items = OrderItem.all.where(order_id: session[:order_id])
   end
+
+  def create
+    # gets the product passed in from the link_to on the products/show page.. how?!
+    @product = Product.find(params[:id])
+    @order_items = OrderItem.create(order_id: session[:order_id], product_id: params[:product_id])
+    redirect_to cart_path
+  end
+
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,14 @@
 class OrdersController < ApplicationController
+
+  # wait, what are we using this for? we don't need a list of all orders, do we?
   def index
-    @cart = Cart.find(params[:id])
-    @orders = Order.new
+    @order = Order.find(session[:order_id])
   end
+
+  # def show_cart
+  #   @order = Order.find(session[:order_id])
+  #   @order_items = OrderItem.all.where(order_id: session[:order_id])
+  #   render :cart
+  # end
+
 end
