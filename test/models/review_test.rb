8b9aaead5f27afd3aff8e2d7ed1815439b828fdc
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-05 15:45:53 -0700

    pseudocode test cases for review

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,6 +1,23 @@
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
+
+  test "review belongs to a product" do
+    assert true
+  end
+
+  test "review cant exist without a product" do
+    assert true
+  end
+
+  test "review has a rating" do
+    assert true
+  end
+
+  test "review cant exist without a rating" do
+    assert true
+  end
+
   # test "the truth" do
   #   assert true
   # end
