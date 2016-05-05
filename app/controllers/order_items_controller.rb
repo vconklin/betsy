c6b09fdf44dd5c4dbcc51dd8970d03a7c2d83ffe
Valerie <val.conklin@gmail.com>
2016-05-05 11:26:32 -0700

    Got the cart persisting through the session but the product_id isn't passing on

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,6 @@
 class OrderItemsController < ApplicationController
 
+  # this is the cart page
   def index
     @order = Order.find(session[:order_id])
     @order_items = OrderItem.all.where(order_id: session[:order_id])
@@ -7,8 +8,7 @@ class OrderItemsController < ApplicationController
 
   def create
     # gets the product passed in from the link_to on the products/show page.. how?!
-    @product = Product.find(params[:id])
-    @order_items = OrderItem.create(order_id: session[:order_id], product_id: params[:product_id])
+    @order_item = OrderItem.create(order_id: session[:order_id], product_id: params[:product_id])
     redirect_to cart_path
   end
 
