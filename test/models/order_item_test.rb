cc384bd9e575e8c4d328ad70472cdec580736037
Valerie <val.conklin@gmail.com>
2016-05-06 11:08:53 -0700

    Working on fixtures and tests for oder_items model

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -4,4 +4,10 @@ class OrderItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "an order item must have a quantity" do
+    assert_not_nil order_items(:beyonce_air_item).quantity
+  end
+
+  
 end
