86abb0de3ed23c676d5a79068890876f4a28166d
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-06 13:04:55 -0700

    reviews testing in progress

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
 
-  test "should not save review without rating" do
-    review = Review.new
-    assert_not review.save, "Saved the review without a rating"
-  end
-
-  test "should save a review with a rating" do
+  test "should save a review with a rating and product_id" do
     assert reviews(:review).save, "Did not save the review with a rating"
   end
 
-  test "review belongs to a product" do
-    assert_same reviews(:review).product, products(:product)
+  test "should not save review without rating but with product" do
+    review = Review.new(product_id: 1)
+    assert_not review.save, "Saved the review without a rating"
   end
 
-  test "review must belong to a product" do
-
+  test "should not save review without product but with rating" do
+    review = Review.new(rating: 3)
+    assert_not review.save, "Saved the review without a product"
   end
-
   #
+  # test "review must belong to a product" do
+  #   assert_not reviews(:review_with_no_product).valid?, "Saved the review without a product"
+  # end
+
   # test "review cant exist without a product" do
   #   assert true
   # end
