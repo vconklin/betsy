256708a666f6d9d27fcc17740414a9aaff506418
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-06 10:58:01 -0700

    some tests set up to pass

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -2,23 +2,33 @@ require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
 
+  test "should not save review without rating" do
+    review = Review.new
+    assert_not review.save, "Saved the review without a rating"
+  end
+
+  test "should save a review with a rating" do
+    assert reviews(:review).save, "Did not save the review with a rating"
+  end
+
   test "review belongs to a product" do
-    assert true
+    assert_same reviews(:review).product, products(:product)
   end
 
-  test "review cant exist without a product" do
-    assert true
+  test "review must belong to a product" do
+
   end
 
-  test "review has a rating" do
-    assert true
-  end
-
-  test "review cant exist without a rating" do
-    assert true
-  end
-
-  # test "the truth" do
+  #
+  # test "review cant exist without a product" do
+  #   assert true
+  # end
+  #
+  # test "review has a rating" do
+  #   assert true
+  # end
+  #
+  # test "review cant exist without a rating" do
   #   assert true
   # end
 end
