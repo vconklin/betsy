fc32ec465f2da8738818e06e280cd8fcaa8553b0
Valerie <val.conklin@gmail.com>
2016-05-12 16:34:59 -0700

    come on heroku work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.serve.static_assets = true
+  config.assets.initialize_on_precompile = false
   config.assets.compile = true
 end
