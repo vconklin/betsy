10b2a6d1924ccf8af086943d9cd33a7694e1cf38
Anna Wilson <annie.marie.mason@gmail.com>
2016-05-12 15:34:32 -0700

    Search drop down works

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,5 @@ class Product < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: {only_integer: true, greater_than: 0}
 
+
 end
