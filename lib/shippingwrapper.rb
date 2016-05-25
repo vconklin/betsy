390d312b918e25c0174af30134b75744454da4ba
melissajimison <melissajimison@gmail.com>
2016-05-25 15:44:39 -0700

    change the url for post request

--- lib/shippingwrapper.rb
+++ lib/shippingwrapper.rb
@@ -4,9 +4,7 @@ module ShippingWrapper
   BASE_URL = "http://localhost:3001"
 
   def self.response(products, place, origin)
-    body = { products_specs: products, destination: place, origin: origin }.to_json
-    data = HTTParty.post(BASE_URL+"/products", { body: body}).parsed_response
-
+    data = HTTParty.post(BASE_URL+"/products/", body: { :products_specs => products, :destination => place, :origin => origin }.to_json).parsed_response
   end
 
 end
